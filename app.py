c1fc0d0e102401d8880331de7c0d22384ec2f7fb
AboubacarSiriki <sidiksoum344@gmail.com>
2024-04-24 15:32:13 +0000

    Page accueil

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ bcrypt = Bcrypt(app)
 
 UPLOAD_FOLDER = 'static/image/upload'  # Remplacez par le chemin de votre choix
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-<<<<<<< Updated upstream
+
 # hashed_password = bcrypt.generate_password_hash('kra1234')
 # print(hashed_password)
 
@@ -37,24 +37,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # # Exécutez la requête avec les valeurs
 # cursor.execute(sql, values)
 # conn.commit()
-=======
 
-hashed_password = bcrypt.generate_password_hash('kra1234')
-print(hashed_password)
-
-cursor = conn.cursor()
-# Exécutez la requête SQL en utilisant des paramètres pour éviter les injections SQL
-sql = "INSERT INTO administrateur (nom, prenom, email, mot_pass, telephone, login) VALUES (%s, %s, %s, %s, %s, %s)"
-values = ('Kra', 'Adephe', 'adelphekra@gmail.com', hashed_password, '586954455', 'kra1234')
-
-# Exécutez la requête avec les valeurs
-cursor.execute(sql, values)
-conn.commit()
->>>>>>> Stashed changes
 # ===================================Admin espace ==============================
 
 # Définir une route et la fonction associée
+
 @app.route('/')
+def accueil():
+    # Rendre le template index.html
+    return render_template('accueil.html')
+
+@app.route('/login')
 def login():
     # Rendre le template index.html
     return render_template('connexion/login.html')
@@ -251,8 +244,6 @@ def equipe():
     cursor.close()
 
     return render_template('membres/equipe.html', utilisateurs=utilisateurs)
-
-
 
 
 @app.route('/userlogin', methods=['GET', 'POST'])
